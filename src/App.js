b0cc4dd843127a93670a404092b583941d218ddd
ali.sh <narenje20142014@gmail.com>
2021-09-05 22:22:23 +0430

    start prj

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from "./pages";
+
+function App() {
+  return (
+    <>
+      <h1>app</h1>
+    </>
+  );
+}
+
+export default App;
