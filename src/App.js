45e27c4c3cea72d114d1b19f4d59ca3357a55944
ali.sh <narenje20142014@gmail.com>
2021-09-05 23:28:04 +0430

    route

--- src/App.js
+++ src/App.js
@@ -16,9 +16,20 @@ import {
 
 function App() {
   return (
-    <>
-      <h1>app</h1>
-    </>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/About" exact component={About}></Route>
+        <Route path="/Cart" exact component={Cart}></Route>
+        <Route path="/Products" exact component={Products}></Route>
+        <Route path="/Checkout" exact component={Checkout}></Route>
+        <Route path="/details/:id" exact component={SingleProduct}></Route>
+        <Route path="*" exact component={Error}></Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
