3765da6d029ae8cea269b9e926234268ca0e051e
ali.sh <narenje20142014@gmail.com>
2021-09-25 15:34:01 +0330

    continue design cart unit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,36 @@
 import React from "react";
 import styled from "styled-components";
-import { formatPrice } from "../utils/helpers";
 import AmountButtons from "./AmountButtons";
 import { FaTrash } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 
-const CartItem = ({ id}) => {
- 
-  
+const CartItem = ({ id, name, image, color, price, amount }) => {
+  const { removeItem, toggleAmounts } = useCartContext();
+  const increase = () => {};
+  const decrease = () => {};
   return (
-    <div className="">
-      <h2>cart items</h2>
-    </div>
+    <Wrapper>
+      <div className="title">
+        <img src={image} alt={name} />
+        <div>
+          <h5 className="name">{name}</h5>
+          <p className="color">
+            color: <span style={{ background: color }}></span>
+          </p>
+          <h5 className="price-small">{price / 100}</h5>
+        </div>
+      </div>
+      <h5 className="price">{price / 100}</h5>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+      <h5 className="subtotal">{(price * amount) / 100}</h5>
+      <button
+        type="button"
+        className="remove-btn"
+        onClick={() => removeItem(id)}
+      >
+        <FaTrash />
+      </button>
+    </Wrapper>
   );
 };
 
